b03bbc113d9eb83f2d788bc89b7f6189d1227578
Valdemardelius <vshudrik@gmail.com>
2022-12-06 21:35:05 +0300

    fourth comment
    wrote a couple of text styles

--- components/text/styled.js
+++ components/text/styled.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
-const StyledText = styled.span``
+const StyledText = styled.span`
+color: ${(props) => props.color || "#000000"};
+font-size: ${(props) => props.fontSize || "14px"};
+font-weight: ${(props) => props. fontWeight || "normal"}
+`
 
 export default StyledText;
